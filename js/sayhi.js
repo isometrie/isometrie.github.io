044a64129f6b3bcbb959bcabe20191e67cea9bdc
isometrie <isometrie@outlook.com>
2021-01-25 11:45:15 +0100

    changed filename

--- js/sayhi.js
+++
@@ -1,6 +0,0 @@
-let say = ["hey", "hello", "hi"];
-
-var rnd = Math.ceil(Math.random() * say.length) - 1;
-var sayhi = document.getElementsByClassName("sayhi")[0];
-
-sayhi.innerHTML = "Say " + say[rnd];
