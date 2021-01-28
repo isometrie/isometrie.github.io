d1bf1a4d10ede66718d2804361bb9f68422d325b
isometrie <isometrie@outlook.com>
2021-01-28 16:31:38 +0100

    removed string

--- js/sayhello.js
+++ js/sayhello.js
@@ -3,4 +3,4 @@ let say = ["Hey", "Hello", "Hi"];
 var rnd = Math.ceil(Math.random() * say.length) - 1;
 var sayhi = document.getElementsByClassName("sayhello")[0];
 
-sayhi.innerHTML = "Say " + say[rnd];
+sayhi.innerHTML = say[rnd];
